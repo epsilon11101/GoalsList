776f113a216cd2a895dacba05af7fab2fb9703dd
Aaron MV <epsilon11101@gmail.com>
2023-02-25 12:02:42 -0600

    fix event on btn

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.css";
 
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onCL}>
+    <button type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
